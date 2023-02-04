77d428e39d3237579d9ff232ba5eaaa87adeeba7
Cfpl <cfpl@qq.com>
2023-02-04 16:10:14 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -10,6 +10,8 @@ package com.atguigu;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git !");
+        System.out.println("hello git2 !");
+
     }
 }
 
