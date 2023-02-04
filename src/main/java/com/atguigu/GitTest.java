d7ed6bee26da6e60edec5da1298ff94eb8ab728d
UpSir01 <124247046+UpSir01@users.noreply.github.com>
2023-02-04 21:02:41 +0800

    pull test

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -17,7 +17,7 @@ public class GitTest {
         System.out.println("我不爱你");
         System.out.println("push test");
         System.out.println("push test2");
-
+        System.out.println("pull test");
 
     }
 }
