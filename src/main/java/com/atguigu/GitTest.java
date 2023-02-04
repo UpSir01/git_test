b1150de39544f16b909cd54b7dc97916642b6e7e
Cfpl <cfpl@qq.com>
2023-02-04 20:52:49 +0800

    push commit2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("我爱你");
         System.out.println("我不爱你");
         System.out.println("push test");
+        System.out.println("push test2");
 
 
     }
